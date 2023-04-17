229c7aa9cf1bc8634270ad3dcb21930c4580ce00
Nitesh Bhardwaj <nitesh1408922outlook.com>
2023-04-17 16:41:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello, world');
+});
+
+app.listen(8080,function(){
+    console.log('Example app listing on port 8080')
+});
+
+module.exports = app;l̥
